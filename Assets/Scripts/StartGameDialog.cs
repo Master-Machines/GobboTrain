7fe4602ad2ed8099c24094fff05dbc33e80712ab
ceemac <chrismcmanus3d@gmail.com>
2017-07-14 17:15:52 -0500

    hardcore

--- Assets/Scripts/StartGameDialog.cs
+++ Assets/Scripts/StartGameDialog.cs
@@ -15,8 +15,7 @@ public class StartGameDialog : DialogController {
         hudHeight = hudRt.rect.height;
         hudWidth = hudRt.rect.width;
         Total.text = Global.Instance.currency.ToString() + "c";
-        Global.Instance.Load();
-    }
+	}
 
 	public void TiltControls(bool tiltEnabled) {
 		Global.Instance.tiltEnabled = tiltEnabled;
