2297b41570bbca3d7ad262a6b82392a8acd6097c
ceemac <chrismcmanus3d@gmail.com>
2017-08-04 16:13:50 -0500

    productionBuildPush

---
+++ Assets/adsScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleMobileAds;
+using GoogleMobileAds.Api;
+
+public class adsScript : MonoBehaviour {
+
+    public BannerView bannerView;
+
+	// Use this for initialization
+	void Start () {
+        RequestBanner();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void RequestBanner()
+    {
+    #if UNITY_EDITOR
+        string adUnitId = "unused";
+#elif UNITY_ANDROID
+        string adUnitId = "ca-app-pub-5762462122157243/1144769715";
+#elif UNITY_IPHONE
+        string adUnitId = "INSERT_IOS_BANNER_AD_UNIT_ID_HERE";
+#else
+        string adUnitId = "unexpected_platform";
+#endif
+
+        // Create a 320x50 banner at the top of the screen.
+        bannerView = new BannerView(adUnitId, AdSize.Banner, AdPosition.Top);
+    // Create an empty ad request.
+    AdRequest request = new AdRequest.Builder().Build();
+    // Load the banner with the request.
+    bannerView.LoadAd(request);
+}
+    void OnDestroy()
+    {
+        bannerView.Destroy();
+    }
+}
